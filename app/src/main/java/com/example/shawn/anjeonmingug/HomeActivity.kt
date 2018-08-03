04715aabcd4ba073423fcd9e27370774d15a369e
soicem <knq512412@gmail.com>
2018-08-04 02:00:11 +0900

    map : get current location

--- app/src/main/java/com/example/shawn/anjeonmingug/HomeActivity.kt
+++ app/src/main/java/com/example/shawn/anjeonmingug/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.shawn.anjeonmingug
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.location.LocationProvider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
@@ -17,42 +19,54 @@ import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.activity_home.*
 import net.daum.mf.map.api.MapPoint
 
-class HomeActivity() : AppCompatActivity(), LocationListener{
+class HomeActivity() : AppCompatActivity(), LocationListener {
+    var latitude : Double? = null
+    var longitude : Double? = null
+    var mapView : MapView? = null
     override fun onLocationChanged(p0: Location?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println(p0!!.latitude)
+        println(p0!!.longitude)
+        latitude = p0!!.latitude
+        longitude = p0!!.longitude
+        this.mapView!!.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude!!, longitude!!), true);
+
     }
 
+
+
     override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onProviderEnabled(p0: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
+        println("p0  :::::::::" + p0)
     }
 
     override fun onProviderDisabled(p0: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     var locationManager : LocationManager? =null
 
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 0)
         locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val mapView = MapView(this)
-        mapView.setDaumMapApiKey("6f504f9b73ad280372b2aff0036b6f32")
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 0)
+
+        this.mapView = MapView(this)
+        this.mapView!!.setDaumMapApiKey("6f504f9b73ad280372b2aff0036b6f32")
 
         val container = findViewById<View>(R.id.map_view) as RelativeLayout
         container.addView(mapView)
-
         gps_icon.bringToFront()
-        gps_icon.setOnClickListener(){
 
+
+
+        gps_icon.setOnClickListener(){
+            this.mapView!!.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude!!, longitude!!), true);
         }
+
     }
 
     fun getLocation(){
