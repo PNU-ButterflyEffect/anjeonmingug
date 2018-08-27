d962715126711e3d5655bb86bddc606e6b5e7695
suhwan <lionwithnomane@gmail.com>
2018-08-27 18:26:03 +0900

    infoboard N reviewboard

---
+++ app/src/main/java/com/example/shawn/anjeonmingug/reviewboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shawn.anjeonmingug
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class reviewboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reviewboard)
+    }
+}
